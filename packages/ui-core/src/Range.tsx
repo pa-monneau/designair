cf816c84a7da97b7d3df5d7bab90660d320e9035
Pierre-Alexandre Monneau <pierrealexandre.monneau@gmail.com>
2026-07-26 16:20:20 +0200

    style(ui-core): redesign du curseur Range
    Piste plus fine avec remplissage en dégradé (brand-gradient), pastille
    plus grosse avec ombre colorée + retour tactile (hover/active), halo
    de focus confiné à la pastille au lieu d'encadrer toute la piste.
    Demandé par PA après la première utilisation dans La Note (Bi'air).

--- packages/ui-core/src/Range.tsx
+++ packages/ui-core/src/Range.tsx
@@ -23,11 +23,11 @@ const Range = ({
   const style = { "--range-progress": `${progress}%` } as CSSProperties;
 
   return (
-    <span className="relative flex h-8 w-full items-center" style={style}>
-      <span aria-hidden className="absolute h-2 w-full rounded-full bg-line" />
+    <span className="relative flex h-9 w-full items-center" style={style}>
+      <span aria-hidden className="absolute h-1.5 w-full rounded-full bg-line-subtle" />
       <span
         aria-hidden
-        className="absolute h-2 rounded-full bg-brand-primary"
+        className="absolute h-1.5 rounded-full bg-gradient-to-r from-brand-gradient-from to-brand-gradient-to transition-[width] duration-150"
         style={{ width: "var(--range-progress)" }}
       />
       <input
@@ -36,11 +36,14 @@ const Range = ({
         max={max}
         value={normalizedValue}
         className={classNames(
-          "relative h-full w-full cursor-pointer appearance-none bg-transparent focus:outline-none focus-visible:rounded-full focus-visible:shadow-focus",
-          "[&::-webkit-slider-runnable-track]:h-2 [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-transparent",
-          "[&::-moz-range-track]:h-2 [&::-moz-range-track]:rounded-full [&::-moz-range-track]:bg-transparent",
-          "[&::-webkit-slider-thumb]:size-5 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-surface-elevated [&::-webkit-slider-thumb]:bg-brand-primary",
-          "[&::-moz-range-thumb]:size-5 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-2 [&::-moz-range-thumb]:border-surface-elevated [&::-moz-range-thumb]:bg-brand-primary",
+          "relative h-full w-full cursor-pointer appearance-none bg-transparent focus:outline-none",
+          "[&::-webkit-slider-runnable-track]:h-1.5 [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-transparent",
+          "[&::-moz-range-track]:h-1.5 [&::-moz-range-track]:rounded-full [&::-moz-range-track]:bg-transparent",
+          "[&::-webkit-slider-thumb]:size-6 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:border-[3px] [&::-webkit-slider-thumb]:border-surface-elevated [&::-webkit-slider-thumb]:bg-brand-primary [&::-webkit-slider-thumb]:shadow-brand [&::-webkit-slider-thumb]:transition-transform [&::-webkit-slider-thumb]:duration-150",
+          "[&::-moz-range-thumb]:size-6 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-[3px] [&::-moz-range-thumb]:border-surface-elevated [&::-moz-range-thumb]:bg-brand-primary [&::-moz-range-thumb]:shadow-brand [&::-moz-range-thumb]:transition-transform [&::-moz-range-thumb]:duration-150",
+          "[&:hover::-webkit-slider-thumb]:scale-110 [&:hover::-moz-range-thumb]:scale-110",
+          "[&:active::-webkit-slider-thumb]:scale-95 [&:active::-moz-range-thumb]:scale-95",
+          "[&:focus-visible::-webkit-slider-thumb]:shadow-focus [&:focus-visible::-moz-range-thumb]:shadow-focus",
           "disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
